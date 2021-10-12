dafe68a658b3a213c0df1a30d6b4700448978b5b
dartaryan <76628877+dartaryan@users.noreply.github.com>
2021-10-12 13:16:44 +0300

    create mock tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,44 @@
+export const TASKS = [
+  {
+    id: 1,
+    text: 'Brush My Teeth',
+    logo: '🦷',
+    step: 1,
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Drink Coffee',
+    logo: '☕',
+    step: 2,
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Do The Dishes',
+    logo: '🍽',
+    step: 3,
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Pack My Food',
+    logo: '🍱',
+    step: 4,
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Pack My Backpack',
+    logo: '🎒',
+    step: 5,
+    reminder: true,
+  },
+  {
+    id: 6,
+    text: 'Get Ready To The Gym',
+    logo: '👟',
+    step: 6,
+    reminder: true,
+  },
+];
