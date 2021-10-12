ab7a02004c059d890ba34ea176ec4fb57b9f090b
dartaryan <76628877+dartaryan@users.noreply.github.com>
2021-10-12 12:51:10 +0300

    create button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title: string = 'My Morning Routine';
   constructor() { }
 
   ngOnInit(): void {
